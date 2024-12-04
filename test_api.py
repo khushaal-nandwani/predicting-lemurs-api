bfc833b40c547b9311913e326afe52c60f5bf7e3
khushaal-nandwani <khushaal.nandwani@mail.utoronto.ca>
2024-12-03 22:06:50 -0500

    test updated

--- test_api.py
+++ test_api.py
@@ -1,9 +1,17 @@
 import requests
 
-url = "http://0.0.0.0:8000/predict"  # Replace with the actual server URL
+url = "https://lemur-predictions-5a0eb4f74f1f.herokuapp.com/predict"  # Replace with the actual server URL
 
 data = {
-    "data": {}
+    "data": {
+        "animal_id": ["0002", "0007", "0030", "0046"],
+        "sex": ["M", "M", "F", "M"],
+        "birth_type": ["wild", "wild", "wild", "wild"],
+        "genus": ["O", "O", "E", "H"],
+        "species": ["GG", "GG", "COL", "GG"],
+        "age": [3.93, 7.55, 5.55, 1.50],
+        "month_born": ["Jan", "Jan", "Oct", "Dec"]
+    }
 }
 
 response = requests.post(url, json=data)
